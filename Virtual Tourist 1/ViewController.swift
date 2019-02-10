aacca38c74b19552816e28962aa00f0c157bdf59
hind27 <hendanwar27@outlook.com>
2019-02-10 02:24:42 +0200

    Initial Commit

---
+++ Virtual Tourist 1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Virtual Tourist 1
+//
+//  Created by hind on 2/10/19.
+//  Copyright © 2019 hind. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
